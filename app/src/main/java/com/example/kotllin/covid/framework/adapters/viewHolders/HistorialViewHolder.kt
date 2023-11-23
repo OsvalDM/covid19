536842c6986b8e1761d6316aa41c98a0e8798f89
OsvalDM <osvaldvm140303@gmail.com>
2023-11-23 14:06:18 -0600

    Feat: informacion mostrada

--- app/src/main/java/com/example/kotllin/covid/framework/adapters/viewHolders/HistorialViewHolder.kt
+++ app/src/main/java/com/example/kotllin/covid/framework/adapters/viewHolders/HistorialViewHolder.kt
@@ -8,6 +8,8 @@ import com.example.kotllin.covid.databinding.ItemMainBinding
 class HistorialViewHolder (private val binding: ItemMainBinding) : RecyclerView.ViewHolder(binding.root)  {
 
     fun bind(item: Historial, context: Context){
-        binding.TVnew.text = item.new.toString()
+        binding.TVnew.text = "Nuevos: " + item.new.toString()
+        binding.TVdate.text = item.fecha
+        binding.TVtotal.text = "Total" + item.total.toString()
     }
 }
