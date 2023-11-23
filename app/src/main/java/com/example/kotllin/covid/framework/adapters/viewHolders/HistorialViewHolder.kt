fcdb513965bb1f8a22e5117bbe6a793d01d6181a
OsvalDM <osvaldvm140303@gmail.com>
2023-11-23 13:51:54 -0600

    Feat:funcional reclycer

---
+++ app/src/main/java/com/example/kotllin/covid/framework/adapters/viewHolders/HistorialViewHolder.kt
@@ -0,0 +1,13 @@
+package com.example.kotllin.covid.framework.adapters.viewHolders
+
+import android.content.Context
+import androidx.recyclerview.widget.RecyclerView
+import com.example.kotllin.covid.data.network.model.Historial
+import com.example.kotllin.covid.databinding.ItemMainBinding
+
+class HistorialViewHolder (private val binding: ItemMainBinding) : RecyclerView.ViewHolder(binding.root)  {
+
+    fun bind(item: Historial, context: Context){
+        binding.TVnew.text = item.new.toString()
+    }
+}
--- app/src/main/java/com/example/kotllin/covid/framework/views/activities/MainActivity.kt
+++ app/src/main/java/com/example/kotllin/covid/framework/views/activities/MainActivity.kt
@@ -3,14 +3,20 @@ package com.example.kotllin.covid.framework.views.activities
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.kotllin.covid.data.network.model.Historial
 import com.example.kotllin.covid.data.network.model.covid.RegistroItem
 import com.example.kotllin.covid.databinding.ActivityMainBinding
+import com.example.kotllin.covid.framework.adapters.HistorialAdapter
 import com.example.kotllin.covid.framework.viewModels.MainViewModel
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val viewModel: MainViewModel by viewModels()
+
+    private val adapter : HistorialAdapter = HistorialAdapter()
+    private lateinit var data:ArrayList<Historial>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,12 +37,24 @@ class MainActivity : AppCompatActivity() {
         viewModel.covidObjectLiveData.observe(this) { registroObject ->
             if (registroObject != null) {
                 setUpView(registroObject)
+                setUpRecyclerView(registroObject)
             }
         }
     }
 
     private fun setUpView(dataForList:ArrayList<Historial>){
     }
+    private fun setUpRecyclerView(dataForList:ArrayList<Historial>){
+        binding.rvcovid.setHasFixedSize(true)
+        val linearLayoutManager = LinearLayoutManager(
+            this,
+            LinearLayoutManager.VERTICAL,
+            false)
+        binding.rvcovid.layoutManager = linearLayoutManager
+        adapter.HistorialAdapter(dataForList, this)
+        binding.rvcovid.adapter = adapter
+    }
+
 
     private fun initializeListeners(){
 
